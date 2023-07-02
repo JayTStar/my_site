56196dc934980b7e6accff6dd20f301c70e9bbe8
Jorge <jsbatalha@gmail.com>
2023-07-01 23:56:36 -0300

    Creating base header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -3,7 +3,17 @@ import "./index.css";
 export default function Header(){
     return(
         <header>
-            Header
+            <div className="top">
+                <p>Title</p>
+            </div>
+            <div className="bottom">
+                <ul>
+                    <p>Home</p>
+                    <p>Sobre</p>
+                    <p>Projetos</p>
+                    <p>Contatos</p>
+                </ul>
+            </div>
         </header>
     )
 }
