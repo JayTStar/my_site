5320a10029bbdf83443068d660ec1703af57a9ed
Jorge <jsbatalha@gmail.com>
2023-10-20 22:11:13 -0300

    Protect div

--- src/Screens/ProjectsScreen/index.tsx
+++ src/Screens/ProjectsScreen/index.tsx
@@ -1,10 +1,18 @@
 import Header from "../../Components/Header";
 
+import "./index.css"
+
 export default function ProjectsScreen(){
     return(
         <>
             <main>
-                <p>Em breve meus projetos estarão disponíveis aqui!</p>
+                <div className="project">
+                    <div className="img"></div>
+                    <div className="info">
+                        <p className="title">Title</p>
+                        <p className="about">About</p>
+                    </div>
+                </div>
             </main>
         </>
     )
