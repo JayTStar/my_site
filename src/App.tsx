19946d920474fc7a3cca19c59e3022f33c756f06
Jorge <jsbatalha@gmail.com>
2023-10-21 22:00:43 -0300

    Delete home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./Components/Header";
-import HomeScreen from "./Screens/HomeScreen";
 import ProjectsScreen from "./Screens/ProjectsScreen";
 import ContactsScreen from "./Screens/ContactsScreen";
 import AboutScreen from "./Screens/AboutScreen/AboutScreen";
@@ -11,10 +10,9 @@ function App() {
       <BrowserRouter>
       <Header/>
           <Routes>
-            <Route path="/" element={<HomeScreen />} />
+            <Route path="/" element={<AboutScreen />} />
             <Route path="/projects" element={<ProjectsScreen />} />
             <Route path="/contacts" element={<ContactsScreen />} />
-            <Route path="/about" element={<AboutScreen />} />
           </Routes>
       </BrowserRouter>
   );
