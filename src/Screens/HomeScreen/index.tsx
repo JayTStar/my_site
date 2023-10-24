6af3d734352a521d1b7534f4b71792458e2f6415
Jorge Tadeu Soares Batalha <jsbatalha@gmail.com>
2023-10-24 08:28:54 -0300

    Updating home text

--- src/Screens/HomeScreen/index.tsx
+++ src/Screens/HomeScreen/index.tsx
@@ -4,9 +4,8 @@ export default function HomeScreen(){
     return(
         <main>
             <h1>Bem Vindo!</h1>
-
-            <p>Olá!</p>
-            <p>Seja bem vindo(a), meu nome é Jorge Tadeu e esse site foi criado para registrar um pouco da minha vida. Para saber mais da minha trajetória vá para a aba "Sobre". Para conhecer os projetos em que eu trabalhei, vá para a aba "Projetos". E para saber as formas de entrar em contato comigo, vá para a aba "Contatos". </p>
+            <p className="text">Olá!</p>
+            <p className="text">Seja bem vindo(a), meu nome é Jorge Tadeu e esse site foi criado para registrar um pouco da minha vida. Para saber mais da minha trajetória vá para a aba "Sobre". Para conhecer os projetos em que eu trabalhei, vá para a aba "Projetos". E para saber as formas de entrar em contato comigo, vá para a aba "Contatos". </p>
         </main>
     )
 }
